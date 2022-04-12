859f82190617820e2bbf35a66f8bf53d6e2482ae
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2022-04-12 20:52:00 +0300

    Update serv.py

--- serv.py
+++ serv.py
@@ -9,7 +9,7 @@ try:
     # Добавляем необходимые подклассы - MIME-типы
     import subprocess
     import os.path
-    import socket, cv2
+    import socket
     import stat
     import os, time
 
@@ -70,36 +70,6 @@ try:
         thread.start()
     """
 
-
-    def CalcImageHash(FileName):
-        image = cv2.imread(FileName)  # Прочитаем картинку
-        resized = cv2.resize(image, (8, 8), interpolation=cv2.INTER_AREA)  # Уменьшим картинку
-        gray_image = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)  # Переведем в черно-белый формат
-        avg = gray_image.mean()  # Среднее значение пикселя
-        ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0)  # Бинаризация по порогу
-
-        # Рассчитаем хэш
-        _hash = ""
-        for x in range(8):
-            for y in range(8):
-                val = threshold_image[x, y]
-                if val == 255:
-                    _hash = _hash + "1"
-                else:
-                    _hash = _hash + "0"
-
-        return _hash
-
-
-    def CompareHash(hash1, hash2):
-        l = len(hash1)
-        i = 0
-        count = 0
-        while i < l:
-            if hash1[i] != hash2[i]:
-                count = count + 1
-            i = i + 1
-        return count
 
 
     def read_file(command):
